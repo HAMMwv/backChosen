3f7b2f6e47431ea9b30a9406e54d00fb71f969e5
Henry <hamoncadam@unal.edu.co>
2023-03-23 19:00:34 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,58 @@
+require('./mongo.js')
+
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const port = process.env.PORT || 3001;
+
+const ChosenModel = require('./models/ChosenModel')
+
+
+app.use(cors());
+app.use(express.json());
+
+/**
+ * retorta la lista de los posibles votantes
+ */
+// app.get('/api/posiblesVotantes', (req, res) => {
+//   VoterAdmin.find()
+//     .then((datos) => {
+//       res.json(datos);
+//     })
+// })
+
+
+/**
+ * añade un votante a la lista votantes
+ */
+app.post('/api/chosenRegister', (req, res) => {
+  const data = req.body;
+  if (!data) {
+    return res.status(400).json({
+      error: 'chosen is missing'
+    });
+  }
+  const chosen = new ChosenModel(req.body);
+  chosen.save()
+    .then(
+      res.json({
+        respuesta: 'Elegido Registrado'
+      })
+    )
+    .catch((err) => {
+      console.log(err);
+    })
+})
+
+
+
+app.use((req, res) => {
+  return res.status(404).json({
+    error: 'not found service reques'
+  })
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ models/ChosenModel.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const ChosenModelSchema = new Schema({
+    address_city : String,
+    address_street: String,
+    birthdate: String,
+    email: String,
+    foreign_person: String,
+    gender: String,
+    first_name: String,
+    last_name: String,
+    ocupation: String,
+    phone: {
+        country:String,
+        value:String,
+    },
+    photo: {
+        url:String,
+        file: Map,    
+    },
+    
+});
+
+ChosenModelSchema.set('toJSON',{
+    transform: (documento, returnedObject)=>{
+        returnedObject.id = returnedObject._id;
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+})
+
+const ChosenModel = model('ChosenModel', ChosenModelSchema);
+
+module.exports = ChosenModel;
